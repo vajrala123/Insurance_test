15b004a3841a1ffc926293bd6b859e43c03dc0f4
vajrala123 <mytempvajrala@gmail.com>
2024-03-06 01:17:23 -0500

    committed

---
+++ src/main/java/com/insurance/insurance_test/App.java
@@ -0,0 +1,70 @@
+package com.insurance.insurance_test;
+
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By.ByCssSelector;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public static void main( String[] args ) throws InterruptedException
+	    {
+	                
+	        	        
+		    WebDriverManager.chromedriver().setup();
+		    
+	        ChromeOptions chromeoptions = new ChromeOptions();
+	        //chromeoptions.addArguments("--headless");
+	        
+	        WebDriver driver = new ChromeDriver(chromeoptions);
+	        
+	        System.out.println("Script Execution Started...");
+	        
+	        driver.get("http://18.118.213.148:8081/contact.html");
+	        
+	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	        
+	        
+	        
+	        Thread.sleep(1000);
+	        
+	        driver.findElement(By.id("inputName")).sendKeys("Vajrala");
+	        
+	        Thread.sleep(1000);
+	        
+	        driver.findElement(By.id("inputNumber")).sendKeys("991111");
+	        
+	        Thread.sleep(1000);
+	        
+	        driver.findElement(By.id("inputMail")).sendKeys("admin@xyz.com");
+	        
+	        Thread.sleep(1000);
+	        
+	        driver.findElement(By.id("inputMessage")).sendKeys("I am interested");
+	        
+	        Thread.sleep(1000);
+	        
+	        
+	        driver.findElement(By.id("my-button")).click();
+	        
+	        //driver.findElement(By.className("btn-box")).click();
+	                          
+	       	Thread.sleep(3000);
+	        
+	        System.out.println("Sent - Execution Completed");
+	        
+	        driver.quit();
+	    }
+}
+
